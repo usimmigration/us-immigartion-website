2086ad03ad40a487eb74eed04bd90762536d962b
ostapenko-kostia <ostapenkokpersonal@gmail.com>
2025-08-09 12:47:24 +0300

    fix

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -1,50 +1,10 @@
-'use client'
+import { Application } from './Application'
 
-import { useState, useEffect } from 'react'
-import { useSearchParams, useRouter } from 'next/navigation'
-import { TRANSLATIONS, LANGS } from './application.data'
-import {
-	LanguageSelector,
-	ApplicationHero,
-	ApplicationTrust,
-	ApplicationService,
-	ApplicationCountries,
-	ApplicationInformation,
-	ApplicationAccreditation,
-} from '@/components/application'
-
-export default function EstaInfoPage() {
-	const searchParams = useSearchParams()
-	const router = useRouter()
-	const langFromQuery = searchParams.get('lang')
-
-	const [lang, setLang] = useState('en')
-
-	useEffect(() => {
-		if (langFromQuery && LANGS.some(l => l.code === langFromQuery)) {
-			setLang(langFromQuery)
-		} else {
-			setLang('en')
-			const url = new URL(window.location.href)
-			url.searchParams.set('lang', 'en')
-			router.replace(url.toString())
-		}
-	}, [langFromQuery])
-
-	const t = TRANSLATIONS[lang as keyof typeof TRANSLATIONS]?.statsTitle
-		? TRANSLATIONS[lang as keyof typeof TRANSLATIONS]
-		: TRANSLATIONS.en
-
-	return (
-		<main className='min-h-screen bg-gradient-to-b from-blue-50 to-white text-gray-900 font-sans p-6 max-w-7xl mx-auto'>
-			<LanguageSelector lang={lang} setLang={setLang} />
-
-			<ApplicationHero t={t} />
-			<ApplicationTrust t={t} />
-			<ApplicationService t={t} />
-			<ApplicationCountries t={t} />
-			<ApplicationInformation t={t} />
-			<ApplicationAccreditation t={t} />
-		</main>
-	)
+export default async function EstaInfoPage({
+	searchParams,
+}: {
+	searchParams: Promise<{ lang: string }>
+}) {
+	const lang = (await searchParams).lang
+	return <Application langFromQuery={lang} />
 }
